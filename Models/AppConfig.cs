72d9f05b4f71b5c70394bf77d5c687c04f73a3e5
GatiZapo <toniruizblascos@gmail.com>
2026-08-25 21:10:19 +0200

    0.3: add version, Switchroot metadata and update settings

--- Models/AppConfig.cs
+++ Models/AppConfig.cs
@@ -2,11 +2,13 @@ namespace MewSwitchManager.Models;
 
 public sealed class AppConfig
 {
+    public string AppVersion { get; set; } = "0.3.0-alpha";
     public LinuxImageConfig LinuxImage { get; set; } = new();
     public StorageConfig Storage { get; set; } = new();
     public SafetyConfig Safety { get; set; } = new();
     public DependencyConfig Dependencies { get; set; } = new();
     public UiConfig Ui { get; set; } = new();
+    public UpdateConfig Updates { get; set; } = new();
 }
 
 public sealed class LinuxImageConfig
@@ -15,6 +17,7 @@ public sealed class LinuxImageConfig
     public string FileName { get; set; } = "theofficialgman-kubuntu-noble-5.1.2-2026-05-13.7z";
     public string Sha1 { get; set; } = "66e6d4990df4d671e64615b7ec6276299fdebc21";
     public long ExpectedSizeBytes { get; set; } = 2374622000L;
+    public string LinuxDistroVersion { get; set; } = "Ubuntu Noble / Switchroot 5.1.2";
 }
 
 public sealed class StorageConfig
@@ -41,4 +44,11 @@ public sealed class UiConfig
     public string Accent { get; set; } = "Mew";
     public bool StartMaximized { get; set; } = false;
     public bool RememberWindowBounds { get; set; } = true;
+}
+
+public sealed class UpdateConfig
+{
+    public bool Enabled { get; set; } = true;
+    public string Repository { get; set; } = "GatiZapo/MewSwitchManager";
+    public bool CheckOnStartup { get; set; } = true;
 }
